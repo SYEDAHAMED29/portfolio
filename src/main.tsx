b55c7696779c9755ceb51e7dfda2898c7fe78158
SYED AHAMED <syedmadh23@gmail.com>
2026-02-21 03:08:56 +0530

    FIX: local events will not be counted in posthog

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { PostHogProvider } from "@posthog/react";
 
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  defaults: "2026-01-30",
+  opt_out_capturing_by_default: !import.meta.env.PROD,
 } as const;
 
 createRoot(document.getElementById("root")!).render(
